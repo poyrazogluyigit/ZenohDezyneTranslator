6da16e656922b2d3bd8ec9133a1dc6179fdf79b7
Yigit Kagan Poyrazoglu <poyrazogluyigit@gmail.com>
2025-12-30 12:04:49 +0300

    save before parser change

---
+++ examples/node2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <thread>
+
+#include "Language.h"
+
+using namespace std::literals::chrono_literals;
+
+DSL_ENUM(State, A, B);
+
+State state = State::A;
+
+int main(){
+
+    Wrapper wrapper;
+
+    zenoh::Config conf = zenoh::Config::create_default();
+    wrapper.open(conf);
+
+    // bir alttaki tamamen farazi bir ornek
+    wrapper.register_subint("x", 0, 3);
+    wrapper.register_enum(EnumInfo<State>::get_enum_data(), EnumInfo<State>::get_enum_size());
+    wrapper.add_transition("demo/wrapper/1: x < 3", reflect::to_string(State::A), reflect::to_string(State::B));
+    wrapper.add_transition("demo/wrapper/1", reflect::to_string(State::B), reflect::to_string(State::A));
+
+
+    auto pub = wrapper.declare_publisher("demo/wrapper/2");
+
+    auto c_on_sample = [&pub](const zenoh::Sample &sample){
+            std::cout << sample.get_payload().as_string() << std::endl;
+            pub.put(reflect::to_string(state));
+            if (state == State::A) {
+                state = State::B;
+            }
+            else state = State::A;
+    };
+
+    auto sub = wrapper.declare_subscriber("demo/wrapper/1", c_on_sample, zenoh::closures::none);
+
+    while (1){
+        // pub.put("Sent from 2");
+        std::this_thread::sleep_for(200ms);
+    }
+
+    return 0;
+}
--- src/include/Registries.h
+++ src/include/Registries.h
@@ -10,19 +10,19 @@ enum class VarType {
 };
 
 struct SubintData {
-    const char* name;
+    std::string_view name;
     int* data;
     int min;
     int max;
 };
 
 struct BoolData {
-    const char * name;
+    std::string_view name;
     bool initialValue;
 };
 
 struct EnumData {
-    const char * name;
+    std::string_view name;
     size_t size;
     const std::pair<std::string_view, int> * data;
 };
@@ -41,51 +41,6 @@ struct Entry {
 
 };
 
-
-class Registry {
-public:
-    static Registry& get(){
-        static Registry instance;
-        return instance;
-    }
-    void registerSubint(const char* name, int* ptr, int min, int max){
-        entries.push_back(SubintData{name, ptr, min, max});
-    }
-    void registerEnum(const char * name, size_t size, const std::pair<std::string_view, int> * table) {
-        entries.push_back(EnumData{name, size, table});
-    }
-    void registerBool(const char * name, bool initialValue) {
-        entries.push_back(BoolData{name, initialValue});
-    }
-
-    void clear(){entries.clear();}
-
-    std::vector<Entry>& getAll() {return entries; }
-
-    Registry(Registry&) = delete;
-    Registry(Registry&&) = delete;
-    void operator=(const Registry&) = delete;
-protected:
-    Registry() = default;
-private:
-    std::vector<Entry> entries;
-};
-
-struct AutoRegister {
-    AutoRegister(const char * name, int * ptr, int min, int max){
-        Registry::get().registerSubint(name, ptr, min, max);
-    }
-
-    template <std::size_t N>
-    AutoRegister(const char * name, const std::array<std::pair<std::string_view, int>, N>* arrayPtr) {
-        Registry::get().registerEnum(name, N, arrayPtr->data());
-    }
-
-    AutoRegister(const char * name, bool initialValue) {
-        Registry::get().registerBool(name, initialValue);
-    }
-};
-
 enum class ScopeTokenType {
     BEGIN_SCOPE,
     END_SCOPE,
@@ -99,71 +54,17 @@ struct ScopeToken {
     std::string_view stmt;
 };
 
-struct StmtRegistry {
-    static StmtRegistry& get() {
-        static StmtRegistry instance;
-        return instance;
-    }
-    void beginScope() {
-        this->stream.push_back(ScopeToken{ScopeTokenType::BEGIN_SCOPE, ""});
-    }
-    void endScope() {
-        this->stream.push_back(ScopeToken{ScopeTokenType::END_SCOPE, ""});
-    }
-    void declareFunction(const char* name) {
-        this->stream.push_back(ScopeToken{ScopeTokenType::FUNC_DECL, std::string_view(name)});
-    }
-    void push_stmt(ScopeTokenType type, const char* stmt) {
-        this->stream.push_back(ScopeToken{type, std::string_view(stmt)});
-    }
-    void clear(){stream.clear();}
-    std::vector<ScopeToken>& getAll() {return stream;}
-
-    std::vector<ScopeToken> stream;
-
-};
-
-/*
-struct ScopeAutoTracker {
-    ScopeAutoTracker() {
-        StmtRegistry::get().beginScope();
-    }
-    ~ScopeAutoTracker() {
-        StmtRegistry::get().endScope();
-    }
-
-    ScopeAutoTracker(const ScopeAutoTracker&) = delete;
-    ScopeAutoTracker& operator=(const ScopeAutoTracker&) = delete;
-
-    ScopeAutoTracker(ScopeAutoTracker&&) = delete;
-    ScopeAutoTracker& operator=(ScopeAutoTracker&&) = delete;
-
-};
-*/
-
-struct ScopeAutoReg {
-    ScopeAutoReg(ScopeTokenType type){
-        if (type == ScopeTokenType::BEGIN_SCOPE) StmtRegistry::get().beginScope();
-        else if (type == ScopeTokenType::END_SCOPE) StmtRegistry::get().endScope();
-    }
-    ScopeAutoReg(const char* name) {
-        StmtRegistry::get().declareFunction(name);
-    }
-    ScopeAutoReg(ScopeTokenType type, const char* stmt) {
-        StmtRegistry::get().push_stmt(type, stmt);
-    }
-};
 
 struct ParseContext {
     std::vector<Entry> variables;
     std::vector<ScopeToken> scopes;
-    void registerSubint(const char* name, int* ptr, int min, int max){
+    void registerSubint(std::string_view name, int* ptr, int min, int max){
         variables.push_back(SubintData{name, ptr, min, max});
     }
-    void registerEnum(const char * name, size_t size, const std::pair<std::string_view, int> * table) {
+    void registerEnum(std::string_view name, size_t size, const std::pair<std::string_view, int> * table) {
         variables.push_back(EnumData{name, size, table});
     }
-    void registerBool(const char * name, bool initialValue) {
+    void registerBool(std::string_view name, bool initialValue) {
         variables.push_back(BoolData{name, initialValue});
     }
     void beginScope() {
